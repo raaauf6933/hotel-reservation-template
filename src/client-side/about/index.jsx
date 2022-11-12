1c6347de6507694f68bddbf0479260e8105061a7
raaauf6933 <adimaampao@stratpoint.com>
2022-11-13 02:32:55 +0800

    added policies

--- src/client-side/about/index.jsx
+++ src/client-side/about/index.jsx
@@ -6,6 +6,7 @@ import AppContainer from "../components/AppContainer";
 import AppLayout from "../components/AppLayout";
 import Hero from "../components/Hero";
 import { Typography, Box } from "@mui/material";
+import image_1 from "../../assets/images/business_permit.png";
 
 const About = () => {
   return (
@@ -23,47 +24,71 @@ const About = () => {
             {" "}
             <Card>
               <CardContent>
-                <Grid container alignItems="center" spacing={3}>
-                  <Grid item md={6} sm={12} xs={12} textAlign="center">
-                    <img
-                      src={`https://res.cloudinary.com/dlqsqlkws/image/upload/v1645010234/GALLERY/17_jfqbss.jpg?w=600&h=700&fit=crop&auto=format`}
-                      srcSet={`https://res.cloudinary.com/dlqsqlkws/image/upload/v1645010234/GALLERY/17_jfqbss.jpg?w=600&h=700&fit=crop&auto=format&dpr=2 2x`}
-                      alt="Villa Gregoria Resort"
-                      loading="lazy"
-                      style={{
-                        boxShadow: "1em",
-                        width: " 100%",
-                        height: "auto",
-                        borderRadius: "5%",
-                      }}
-                    />
+                <div
+                  style={{
+                    padding: "2.5em",
+                  }}
+                >
+                  <Grid container alignItems="center" spacing={3}>
+                    <Grid item md={12} sm={12} xs={12}>
+                      <Typography
+                        variant="h4"
+                        fontWeight={600}
+                        gutterBottom
+                        textAlign="center"
+                      >
+                        Make Your Tour Memorable and Safe With Us
+                      </Typography>
+                      <Typography variant="h6" lineHeight={1.5}>
+                        <b>Villa Gregoria Resort</b> is an accredited resort by
+                        the Department of Tourism, located at Brgy. Buboy
+                        Nagcarlan, Laguna (a two to three-hour drive from Metro
+                        Manila). It provides cool, relaxing and refreshing
+                        environment for everyone who wants to escape the hustle
+                        and bustle of the modern city. Day swimming and
+                        overnight stay are offered for guests who want to have
+                        relaxing moments as they experience the resort's cool
+                        water, and enjoy the resort's sophisticated and
+                        architectural cottages, rooms, pools and pavilion.
+                        Private pool, mini-Olympic pool, water slides, cold
+                        spring / batis, are some of the facilities that every
+                        member of the family can surely enjoy.
+                      </Typography>
+                    </Grid>
+                    <Grid item md={12} sm={12} xs={12} textAlign="center">
+                      <img
+                        src={`https://res.cloudinary.com/dlqsqlkws/image/upload/v1645010234/GALLERY/17_jfqbss.jpg?w=600&h=700&fit=crop&auto=format`}
+                        srcSet={`https://res.cloudinary.com/dlqsqlkws/image/upload/v1645010234/GALLERY/17_jfqbss.jpg?w=600&h=700&fit=crop&auto=format&dpr=2 2x`}
+                        alt="Villa Gregoria Resort"
+                        loading="lazy"
+                        style={{
+                          boxShadow: "1em",
+                          width: " 100%",
+                          height: "auto",
+                          borderRadius: "5%",
+                        }}
+                      />
+                    </Grid>
+
+                    <Grid item md={12} sm={12} xs={12} textAlign="center">
+                      <div
+                        style={{
+                          width: "100%",
+                        }}
+                      >
+                        <img
+                          src={image_1}
+                          style={{
+                            boxShadow: "1em",
+                            width: "100%",
+                            height: "auto",
+                            borderRadius: "5%",
+                          }}
+                        ></img>
+                      </div>
+                    </Grid>
                   </Grid>
-                  <Grid item md={6} sm={12} xs={12}>
-                    <Typography
-                      variant="h4"
-                      fontWeight={600}
-                      gutterBottom
-                      textAlign="center"
-                    >
-                      Make Your Tour Memorable and Safe With Us
-                    </Typography>
-                    <Typography variant="h6" lineHeight={1.5}>
-                      <b>Villa Gregoria Resort</b> is an accredited resort by
-                      the Department of Tourism, located at Brgy. Buboy
-                      Nagcarlan, Laguna (a two to three-hour drive from Metro
-                      Manila). It provides cool, relaxing and refreshing
-                      environment for everyone who wants to escape the hustle
-                      and bustle of the modern city. Day swimming and overnight
-                      stay are offered for guests who want to have relaxing
-                      moments as they experience the resort's cool water, and
-                      enjoy the resort's sophisticated and architectural
-                      cottages, rooms, pools and pavilion. Private pool,
-                      mini-Olympic pool, water slides, cold spring / batis, are
-                      some of the facilities that every member of the family can
-                      surely enjoy.
-                    </Typography>
-                  </Grid>
-                </Grid>
+                </div>
               </CardContent>
             </Card>
           </div>
--- src/client-side/home/index.jsx
+++ src/client-side/home/index.jsx
@@ -18,6 +18,7 @@ import { resortName } from "./../../config";
 import bookingContext from "../context/booking/bookingContext";
 import AppLayout from "../components/AppLayout";
 import ReviewsSection from "../components/ReviewsSection";
+import { Typography } from "@mui/material";
 
 const Home = () => {
   const { navbarDispatch } = React.useContext(navbarContext);
@@ -41,6 +42,99 @@ const Home = () => {
           <DatePickerSection />
           <ServiceSection />
           <ReviewsSection />
+          <div
+            style={{
+              paddingBottom: "2em",
+            }}
+          >
+            <div
+              style={{
+                title: {
+                  padding: "2em",
+                },
+              }}
+            >
+              <Typography variant="h2" textAlign="center">
+                Booking & Hotel Policies
+              </Typography>
+            </div>
+
+            <Typography textAlign={"justify"} marginBottom={2}>
+              - Booking Confirmation: The reservation is considered confirmed
+              when you receive a confirmation thru email containing the
+              reference ID. Present this together with a valid ID upon check-in.
+              NO VOUCHER, NO CHECK-IN.
+            </Typography>
+
+            <Typography textAlign={"justify"} marginBottom={2}>
+              - Booking Modification/Cancellation: Cancellation and modification
+              is not allowed. This booking is 100% confirmed once paid
+            </Typography>
+
+            <Typography textAlign={"justify"} marginBottom={2}>
+              - Prepayment: Full prepayment is required to confirm your
+              reservation. This is nonrefundable.
+            </Typography>
+
+            <Typography textAlign={"justify"} marginBottom={2}>
+              - Modification: Change date or rebooking not allowed.
+              Cancellation: Not Allowed No Show: Your 100% prepayment will be
+              forfeited if you do not arrive within 24 hours of the check-in
+              date and time of the hotel.
+            </Typography>
+
+            <Typography textAlign={"justify"} marginBottom={2}>
+              - Cancellation caused by Acts of Nature & National Events: The
+              resort will not be held liable for services not rendered or for
+              any loss or damage due to natural calamity, flight or boat
+              transfer cancellations due to bad weather condition and national
+              events. If a booking was cancelled by the client because of force
+              majeure, there will be no refund but is re-bookable to a new tour
+              date within 30 days subject to room availability and rate
+              difference if any.
+            </Typography>
+
+            <Typography textAlign={"justify"} marginBottom={2}>
+              - Check-in and Check-out Policy: Check-in time is at 3:00 pm and
+              check-out time is at 12:00 noon. Early check-in or late check-out
+              will depend on room availability and with corresponding charges.
+            </Typography>
+
+            <Typography textAlign={"justify"} marginBottom={2}>
+              Kid Policy: Kids sharing room with parents from 0-4 years old are
+              free.
+            </Typography>
+            <Typography textAlign={"justify"} marginBottom={2}>
+              - Non-smoking Policy: In order to maintain the beauty and comfort
+              of our accommodation, for non-smoking rooms, violators will be
+              charged a penalty fee equivalent to 2 nights based on published
+              rates.
+            </Typography>
+
+            <Typography textAlign={"justify"} marginBottom={2}>
+              - No Show Policy: Full payment will be forfeited if guest do not
+              arrive within 24 hours of the check-in date and time of the hotel.
+            </Typography>
+
+            <Typography textAlign={"justify"} marginBottom={2}>
+              - Online Promo Rate: Online promo rates are not applicable for
+              Senior Citizen and PWD (Persons with disabilities) discounts.
+            </Typography>
+
+            <Typography textAlign={"justify"} marginBottom={2}>
+              - Room Upgrade: Upgrade to a higher category is subject to room
+              availability and rate difference. Full prepayment is required. For
+              inquiries, contact our reservation team + 63 927 541 0656
+            </Typography>
+
+            <Typography textAlign={"justify"} marginBottom={2}>
+              - Special Request/Preferences: Bed type, smoking and non-smoking
+              preference and room allocation are all subject to availability
+              upon check-in. The resort voucher indicates the room and the
+              services you have paid for. Anything that has been overlooked
+              should be paid at the resort.
+            </Typography>
+          </div>
           {/* <SectionLabel title="Best of Our Rooms" />
 
         <div style={{ display: "flex", justifyContent: "center" }}>
