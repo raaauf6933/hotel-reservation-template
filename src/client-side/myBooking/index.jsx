a9456220c7a6e2d07bb73b2b1d309645ed720ab9
raaauf6933 <adimaampao@stratpoint.com>
2022-11-16 14:10:36 +0800

    added mybokoing bg

--- src/client-side/myBooking/index.jsx
+++ src/client-side/myBooking/index.jsx
@@ -7,7 +7,7 @@ import createDialogAction from "./../utils/dialogActionHandlers";
 import { MyBookingPathParamsUrl } from "./url";
 
 // import ConfirmationDialog from "../../admin/components/ConfirmationDialog/ConfirmationDialog";
-
+import bg_transparent from "./../../assets/images/bg_transparent.png";
 const MyBooking = () => {
   const navigate = useNavigate();
   const [open, onClose] = createDialogAction(
@@ -19,20 +19,31 @@ const MyBooking = () => {
 
   return (
     <>
-      <AppLayout awake={true}>
-        <Switch>
-          <Route path="/" element={<MyBookingLogIn />} />
-          <Route
-            path=":id"
-            element={
-              <MyBookingDetails
-                onModifyBooking={() => open("onModifyBooking")}
-                onClose={onClose}
-              />
-            }
-          />
-        </Switch>
-      </AppLayout>
+      <div
+        style={{
+          background: `url("${bg_transparent}")`,
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          minHeight: "100vh",
+          flexDirection: "column",
+        }}
+      >
+        <AppLayout awake={true}>
+          <Switch>
+            <Route path="/" element={<MyBookingLogIn />} />
+            <Route
+              path=":id"
+              element={
+                <MyBookingDetails
+                  onModifyBooking={() => open("onModifyBooking")}
+                  onClose={onClose}
+                />
+              }
+            />
+          </Switch>
+        </AppLayout>
+      </div>
     </>
   );
 };
--- src/client-side/myBooking/views/myBookingLogin.jsx
+++ src/client-side/myBooking/views/myBookingLogin.jsx
@@ -14,6 +14,7 @@ import { MyBookingTypePath } from "../url";
 import { AUTH_CLIENT } from "../api";
 import { LoadingButton } from "@mui/lab";
 
+
 const MyBookingLogIn = () => {
   const [form, setForm] = useState({ booking_reference: "", email: "" });
   const [formError, setFormError] = useState(false);
@@ -44,63 +45,68 @@ const MyBookingLogIn = () => {
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        height: "100%",
-        alignItems: "center",
-        textAlign: "center",
-        marginTop: "5em",
-      }}
-    >
-      <Box sx={{ boxShadow: 3 }}>
-        {" "}
-        <Card>
-          <CardHeader title="My Booking" />
-          <CardContent>
-            <form action="#" class=" pr-5 pl-5 pb-3 contact-form">
-              <div class="form-group">
-                <TextField
-                  fullWidth
-                  autoFocus
-                  label="Booking Reference"
-                  variant="outlined"
-                  value={form.booking_reference}
-                  onChange={(e) =>
-                    setForm({ ...form, booking_reference: e.target.value })
-                  }
-                  error={formError}
-                  helperText={
-                    formError ? "Invalid Booking Reference or Email" : ""
-                  }
-                />
-              </div>
-              <div class="form-group">
-                <TextField
-                  fullWidth
-                  label="Email"
-                  variant="outlined"
-                  value={form.email}
-                  onChange={(e) => setForm({ ...form, email: e.target.value })}
-                />
-              </div>
-              <div class="form-group">
-                <LoadingButton
-                  disabled={!form.booking_reference || !form.email}
-                  variant="contained"
-                  size="large"
-                  onClick={handleLogin}
-                  loading={loading}
-                >
-                  <b>Submit</b>
-                </LoadingButton>
-              </div>
-            </form>
-          </CardContent>
-        </Card>{" "}
-      </Box>
-    </div>
+    <>
+   
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          height: "100%",
+          alignItems: "center",
+          textAlign: "center",
+          marginTop: "5em",
+        }}
+      >
+        <Box sx={{ boxShadow: 3 }}>
+          {" "}
+          <Card>
+            <CardHeader title="My Booking" />
+            <CardContent>
+              <form action="#" class=" pr-5 pl-5 pb-3 contact-form">
+                <div class="form-group">
+                  <TextField
+                    fullWidth
+                    autoFocus
+                    label="Booking Reference"
+                    variant="outlined"
+                    value={form.booking_reference}
+                    onChange={(e) =>
+                      setForm({ ...form, booking_reference: e.target.value })
+                    }
+                    error={formError}
+                    helperText={
+                      formError ? "Invalid Booking Reference or Email" : ""
+                    }
+                  />
+                </div>
+                <div class="form-group">
+                  <TextField
+                    fullWidth
+                    label="Email"
+                    variant="outlined"
+                    value={form.email}
+                    onChange={(e) =>
+                      setForm({ ...form, email: e.target.value })
+                    }
+                  />
+                </div>
+                <div class="form-group">
+                  <LoadingButton
+                    disabled={!form.booking_reference || !form.email}
+                    variant="contained"
+                    size="large"
+                    onClick={handleLogin}
+                    loading={loading}
+                  >
+                    <b>Submit</b>
+                  </LoadingButton>
+                </div>
+              </form>
+            </CardContent>
+          </Card>{" "}
+        </Box>
+      </div>
+    </>
   );
 };
 
